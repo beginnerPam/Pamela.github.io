cb84748e942b113f6720b1038306e6cafe65fc1e
beginnerPam <35022111+beginnerPam@users.noreply.github.com>
2018-04-28 16:39:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$("#wx").mouseover(function(){
+    $("#weixin").css("display","block");
+});
+$("#wx").mouseout(function(){
+    $("#weixin").css("display","none");
+}) ; 
+$("#wb").mouseover(function(){
+    $("#weibo").css("display","block");
+});
+$("#wb").mouseout(function(){
+    $("#weibo").css("display","none");
+}) ; 
+$("#Fc").mouseover(function(){
+    $("#fcc").css("display","block");
+});
+$("#Fc").mouseout(function(){
+    $("#fcc").css("display","none");
+}) ; 
+});
